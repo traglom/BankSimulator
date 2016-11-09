a3db50f6e5cf6ee90de17c0f6fd9ce87e031f2a2
Nicolas <pallas.nicolas@gmail.com>
2016-11-09 17:47:50 +0100

    [+] Gitignore & chemain en relatif

--- InitialisationProjetAvance/src/main/java/com/ujm/xmltech/utils/BankSimulationConstants.java
+++ InitialisationProjetAvance/src/main/java/com/ujm/xmltech/utils/BankSimulationConstants.java
@@ -5,19 +5,19 @@ public class BankSimulationConstants {
   /**
    * Directory where are files to process
    */
-  public final static String IN_DIRECTORY = "/home/clement/Bureau/CorrectionTP/folders/in/";
+  public final static String IN_DIRECTORY = System.getProperty("user.dir")+"/../../folders/in/";
   /**
    * Directory where are reports
    */
-  public final static String OUT_DIRECTORY = "/home/clement/Bureau/CorrectionTP/folders/out/";
+  public final static String OUT_DIRECTORY = System.getProperty("user.dir")+"/../../folders/out/";
   /**
    * Directory where are files under process
    */
-  public final static String WORK_DIRECTORY = "/home/clement/Bureau/CorrectionTP/folders/work/";
+  public final static String WORK_DIRECTORY = System.getProperty("user.dir")+"/../../folders/work/";
   /**
    * Directory where are files already processed
    */
-  public final static String ARCHIVE_DIRECTORY = "/home/clement/Bureau/CorrectionTP/folders/archive/";
+  public final static String ARCHIVE_DIRECTORY = System.getProperty("user.dir")+"/../../folders/archive/";
 
   /**
    * must contain only 4 upper case letters. Real example : BNPP
